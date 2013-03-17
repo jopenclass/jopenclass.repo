2eb4d5def901121409bf7d58bf6978fe053fc053
pathmasri ambegoda <ambegodas@gmail.com>
2013-03-17 09:36:15 +0530

    Student functionality

---
+++ JOpenClass/src/main/java/org/jopenclass/controller/StudentDetailController.java
@@ -0,0 +1,26 @@
+package org.jopenclass.controller;
+
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.springframework.stereotype.Controller;
+/*
+ * 
+ * 
+ */
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class StudentDetailController {
+
+	@RequestMapping(value = "/savelecturer", method = RequestMethod.POST)
+	public @ResponseBody
+	Object saveLecturer(@RequestBody String json) throws JsonParseException,
+			JsonMappingException, IOException {
+		return lecturerService.postSaveLecturer(json);
+	}
+}
