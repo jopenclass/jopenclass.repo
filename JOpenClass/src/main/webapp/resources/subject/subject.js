bb286df04ea46968dcb240c0af3ce16953df7162
madhumal <madhumal@madhumal.testmadmail.com>
2013-05-25 17:27:33 +0530

    code format

--- JOpenClass/src/main/webapp/resources/subject/subject.js
+++ JOpenClass/src/main/webapp/resources/subject/subject.js
@@ -1,23 +1,22 @@
+function insertSubject() {
 
-	function insertSubject() {
+	var subjectName = $('#subjectName').val();
+	var grade = $('#grade').val();
+	var subjectDetails = $('#subjectDetails').val();
 
-		var subjectName = $('#subjectName').val();
-		var grade = $('#grade').val();
-		var subjectDetails = $('#subjectDetails').val();
-
-		$.ajax({
-			type : "POST",
-			url : "/JOpenClass/savesubject",
-			data : "subjectName=" + subjectName + "&grade=" + grade+ "&subjectDetails=" + subjectDetails
-					+ "&id=-1",
-			success : function(response) {
-				$('#info').html(response.message);
-				$('#subjectName').val('');
-				$('#grade').val('');
-				$('#subjectDetails').val('');
-			},
-			error : function(e) {
-				alert('Error: ' + e);
-			}
-		});
-	}
+	$.ajax({
+		type : "POST",
+		url : "/JOpenClass/savesubject",
+		data : "subjectName=" + subjectName + "&grade=" + grade
+				+ "&subjectDetails=" + subjectDetails + "&id=-1",
+		success : function(response) {
+			$('#info').html(response.message);
+			$('#subjectName').val('');
+			$('#grade').val('');
+			$('#subjectDetails').val('');
+		},
+		error : function(e) {
+			alert('Error: ' + e);
+		}
+	});
+}
