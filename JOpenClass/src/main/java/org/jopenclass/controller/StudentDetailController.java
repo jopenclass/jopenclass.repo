d793aff63aa7bd6036f1a90b46c1bb3be8540028
pathmasri ambegoda <ambegodas@gmail.com>
2013-06-09 02:09:05 +0530

    Linking add student page to home page

--- JOpenClass/src/main/java/org/jopenclass/controller/StudentDetailController.java
+++ JOpenClass/src/main/java/org/jopenclass/controller/StudentDetailController.java
@@ -4,16 +4,14 @@
 
 package org.jopenclass.controller;
 
-import java.io.IOException;
-import org.codehaus.jackson.JsonParseException;
-import org.codehaus.jackson.map.JsonMappingException;
+
 import org.jopenclass.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class StudentDetailController {
@@ -22,10 +20,8 @@ public class StudentDetailController {
 	@Autowired
 	private StudentService studentService;
 
-	@RequestMapping(value = "/saveStudent", method = RequestMethod.POST)
-	public @ResponseBody
-	Object saveStudent(@RequestBody String json) throws JsonParseException,
-			JsonMappingException, IOException {							
-		return null;
+	@RequestMapping(value = "/saveStudent", method = RequestMethod.GET)
+	public String viewAddCourse(ModelMap model) {
+		return "student/student";
 	}
 }
